befb806d6944954986d0ebeac35563a5a674b0a5
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-03 09:18:19 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GA_WriteLine_WCram_Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // <---- This is an opening curly
+          //  Console.WriteLine("Does this run?");
+
+            // Console.WriteLine() - Prints a string and moves to the next line
+            // Console.Write() - Prints a string and stays on the same line
+            // "String"
+            // ; - Semi-Colon
+
+            // Console.WriteLine("Hello, Class!");
+            Console.Write("Hello, Class");
+            Console.WriteLine("This is a string");
+
+            // Name is in a Write, Quote is in a WriteLine
+            Console.Write("1.Edsger W.Dijkstra: ");
+            Console.WriteLine("Computer science is no more about computers than astronomy is about telescopes.");
+            Console.WriteLine(); // A WriteLine without anything in the parentheses will give you a blank space
+            Console.Write("2.Alan Kay: ");
+            Console.WriteLine(" The best way to predict the future is to invent it.");
+
+        } // <---- Closing Main Curly Brace
+
+    } // End class
+
+} // End namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# GA_WriteLine_WCram_Temp
+ 
